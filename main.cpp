eab5b018a4218aad4072df2a686e741679963b1f
vinhomes1311 <vinhdang2006hd@gmail.com>
2025-03-23 15:37:17 +0700

    update 2332025

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -9,23 +10,31 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int TUAN_WIDTH = 350;
 const int TUAN_HEIGHT = 350;
-const int MOM_WIDTH = 350;
-const int MOM_HEIGHT = 350;
+const int MOM_WIDTH = 450;
+const int MOM_HEIGHT = 450;
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 bool running = true;
-bool altTab = false;
 float progress = 0.0f;
-bool momLooking = false;
-Uint32 momLookStartTime = 0;
-const Uint32 MOM_LOOK_DURATION = 1500;
-const float PROGRESS_INCREASE_RATE = 0.05f;
 bool mousePressed = false;
-
-SDL_Texture* tuanTexture = nullptr;
+bool isplayinggame;
+SDL_Texture* tuanChoi = nullptr;
 SDL_Texture* momTexture = nullptr;
 SDL_Texture* backgroundTexture = nullptr;
+SDL_Texture* tuanHoc = nullptr;
+Mix_Music* studyMusic = nullptr;
+Mix_Music* gameMusic = nullptr;
+bool studyMusicPlaying = false;
+
+// Mẹ animation
+bool momComingIn = false;
+bool momLooking = false;
+bool momGoingOut = false;
+Uint32 momLookStartTime = 0;
+const Uint32 MOM_LOOK_DURATION = 3000; // mẹ nhìn 3s
+float momX = SCREEN_WIDTH; // bắt đầu ngoài màn hình
+const float MOM_SPEED = 2.0f; // Di chuyển chậm hơn
 
 SDL_Texture* LoadTexture(const char* filePath) {
     SDL_Surface* surface = IMG_Load(filePath);
@@ -39,15 +48,21 @@ SDL_Texture* LoadTexture(const char* filePath) {
 }
 
 void InitSDL() {
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     IMG_Init(IMG_INIT_PNG);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
     window = SDL_CreateWindow("Stealth Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     srand(time(0));
 
     backgroundTexture = LoadTexture("room_background.png");
-    tuanTexture = LoadTexture("tuan_back.png");
     momTexture = LoadTexture("mom_stern.png");
+    tuanChoi = LoadTexture("TuanChoiGame.png");
+    tuanHoc = LoadTexture("TuanHocBai.png");
+    studyMusic = Mix_LoadMUS("study.wav");
+    gameMusic = Mix_LoadMUS("choigame.mp3");
+    if (!studyMusic) cerr << "Failed to load study.wav: " << Mix_GetError() << endl;
+    if (!gameMusic) cerr << "Failed to load choigame.mp3: " << Mix_GetError() << endl;
 }
 
 void HandleEvents() {
@@ -66,24 +81,61 @@ void HandleEvents() {
 }
 
 void Update() {
+    isplayinggame = false;
     if (mousePressed) {
-        progress += PROGRESS_INCREASE_RATE;
+        progress += 0.05f;
+        isplayinggame = true;
     }
-    if (!momLooking && rand() % 500 == 0) {
-        momLooking = true;
-        momLookStartTime = SDL_GetTicks();
+
+    if (isplayinggame) {
+        if (Mix_PlayingMusic() == 0 || studyMusicPlaying) {
+            Mix_HaltMusic();
+            Mix_PlayMusic(gameMusic, -1);
+            studyMusicPlaying = false;
+        }
+    } else {
+        if (Mix_PlayingMusic() == 0 || !studyMusicPlaying) {
+            Mix_HaltMusic();
+            Mix_PlayMusic(studyMusic, -1);
+            studyMusicPlaying = true;
+        }
     }
+
+    // Mẹ thò đầu vào thường xuyên hơn
+    if (!momComingIn && !momLooking && !momGoingOut && rand() % 250 == 0) {
+        momComingIn = true;
+        momX = SCREEN_WIDTH;
+    }
+
+    if (momComingIn) {
+        momX -= MOM_SPEED;
+        if (momX <= SCREEN_WIDTH - 500) { // vị trí cố định
+            momComingIn = false;
+            momLooking = true;
+            momLookStartTime = SDL_GetTicks();
+        }
+    }
+
     if (momLooking) {
-        if (mousePressed && SDL_GetTicks() - momLookStartTime >= MOM_LOOK_DURATION) {
+        if (isplayinggame) {
             cout << "GAME OVER" << endl;
             running = false;
         }
         if (SDL_GetTicks() - momLookStartTime >= MOM_LOOK_DURATION) {
             momLooking = false;
+            momGoingOut = true;
         }
     }
+
+    if (momGoingOut) {
+        momX += MOM_SPEED;
+        if (momX >= SCREEN_WIDTH) {
+            momGoingOut = false;
+        }
+    }
+
     if (progress >= 100.0f) {
-        cout << "NEXT LEVEL" << endl;
+        cout << "Well done!" << endl;
         running = false;
     }
 }
@@ -92,11 +144,14 @@ void Render() {
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
 
-    SDL_Rect tuanRect = {SCREEN_WIDTH / 2 - 75 - 40, SCREEN_HEIGHT - 275, TUAN_WIDTH, TUAN_HEIGHT};
-    SDL_RenderCopy(renderer, tuanTexture, NULL, &tuanRect);
+    SDL_Rect tuanRect = {SCREEN_WIDTH / 2 - 400, SCREEN_HEIGHT - 350, TUAN_WIDTH, TUAN_HEIGHT};
+    if (isplayinggame)
+        SDL_RenderCopy(renderer, tuanChoi, NULL, &tuanRect);
+    else
+        SDL_RenderCopy(renderer, tuanHoc, NULL, &tuanRect);
 
-    if (momLooking) {
-        SDL_Rect momRect = {SCREEN_WIDTH - 180 - 40, SCREEN_HEIGHT - 275, MOM_WIDTH, MOM_HEIGHT};
+    if (momComingIn || momLooking || momGoingOut) {
+        SDL_Rect momRect = {(int)momX, SCREEN_HEIGHT - 400, MOM_WIDTH, MOM_HEIGHT};
         SDL_RenderCopy(renderer, momTexture, NULL, &momRect);
     }
 
@@ -108,11 +163,15 @@ void Render() {
 }
 
 void CleanUp() {
-    SDL_DestroyTexture(tuanTexture);
     SDL_DestroyTexture(momTexture);
     SDL_DestroyTexture(backgroundTexture);
+    SDL_DestroyTexture(tuanChoi);
+    SDL_DestroyTexture(tuanHoc);
+    Mix_FreeMusic(studyMusic);
+    Mix_FreeMusic(gameMusic);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    Mix_Quit();
     IMG_Quit();
     SDL_Quit();
 }
